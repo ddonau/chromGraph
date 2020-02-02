8e3be8e946874160711e9f3c30ac1f868ef59466
ddonau <ddonau@protonmail.com>
2020-02-03 00:17:47 +1100

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ import os
 
 files = []
 folders = []
-path = r'C:\Users\damien\Documents\Melbourne\WORK\files'
+path = r'WORK\files'
 for (path, dirnames, filenames) in os.walk(path):
     folders.extend(os.path.join(path, name) for name in dirnames)
     files.extend(os.path.join(path, name) for name in filenames)
